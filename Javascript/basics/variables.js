dde84ef60f0c8301d696b2cb4a1f2a26c5b2b684
John-Paul Smith <jp@johnpaulsmith.co.uk>
2025-04-29 15:22:49 +0100

    tutorial finished

--- Javascript/basics/variables.js
+++ Javascript/basics/variables.js
@@ -1,1 +1,62 @@
-console.log("bugger this for a game of soldiers");
+// random code for tutorial requests....
+
+// console.log(7);
+// console.log("John-Paul");
+// console.log(true);
+
+
+//string operations
+//console.log("Hello World!".toUpperCase());
+//console.log("Hello World!".toLowerCase());
+//console.log("  trim  ".trim());
+//console.log("Hello World!".charAt(3));
+//console.log("Hello World!".slice(3, 6));
+//console.log("Hello".concat("World!"));
+
+// declare a variable and use it
+// let firstName = "John-Paul";
+// console.log(" line 4: ", firstName);
+// firstName="Peter";
+// console.log(" line 6: ", firstName);
+// const surname = "Wayne";
+// console.log(" line 8: ", surname);
+// surname ="wont work"; // this will not work as surname is a constant
+// console.log(" line 10: ", surname);
+
+
+
+// arithmetic operators
+/*
+console.log(5 + 3); // addition
+console.log(5 - 3); // subtraction  
+console.log(5 * 3); // multiplication
+console.log(5 / 3); // division
+console.log(5 % 3); // modulus (remainder)  
+console.log(5 ** 3); // exponentiation (5 to the power of 3)
+*/
+
+/*
+// operators and variables
+let num1 = 4;
+let num2 = 5;
+let result = num1 + num2; // addition
+console.log("The result is: " + result); // string concatenation
+*/
+
+// assignment operators
+/*
+let num1 = 4;
+let num2 = 5;
+let result = num1 + num2; // addition
+result *= 2; // multiply result by 2
+console.log("The result is: " + result); // string concatenation
+*/
+
+// template literals
+let firstName = "John-Paul";
+let lastName = "Wayne";
+let age = 30;
+let greeting = `Hello, my name is ${firstName} ${lastName} and I am ${age} years old.`;
+console.log(greeting); // Output: Hello, my name is John-Paul Wayne and I am 30 years old.
+
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # GrowthProgrammingCourse
-GrowthProgrammingCourse
+Code and stuff for my Level 3 course in Software Development.  You probabyl wont be interested in whats in here!
