4bcf100e9be2a5e68c7f4d22dddaf9e5cd919afd
John-Paul Smith <jp@johnpaulsmith.co.uk>
2025-06-02 10:53:44 +0100

    changes

--- OOP/ConsoleStrings/obj/Debug/net9.0/ConsoleStrings.AssemblyInfo.cs
+++ OOP/ConsoleStrings/obj/Debug/net9.0/ConsoleStrings.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleStrings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15afa9c149d5401d34974e866935c125763f821b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a75c19f7617a1e7ef77777b44d44631116d29898")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleStrings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleStrings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
